a50e45bde06962a4e56dc181c36488b5d0ed680c
Sagar Jadhav <jadhavs11393@gmail.com>
2020-03-01 13:27:02 +0000

    f1: add fun changed

--- calc.py
+++ calc.py
@@ -1,11 +1,5 @@
 def add(x,y):
-    return
-def subtract(x,y):
-    pass
-def multiply(x,y):
-    pass
-def divide(x,y):
-    pass
-def square(x,y):
-    pass
+    return x+y
 
+def subtract(x,y,z):
+    pass x-y-z
